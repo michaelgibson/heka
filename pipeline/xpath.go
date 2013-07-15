4d3bda82b5a626c764c28804afdbccfbc01cece1
Victor Ng <victor@crankycoder.com>
2013-07-15 14:54:59 -0400

    init checkin

---
+++ pipeline/xpath.go
@@ -0,0 +1,28 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2012
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Victor Ng (vng@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
+
+package pipeline
+
+import (
+	"github.com/moovweb/gokogiri"
+)
+
+func run() {
+    defer CheckXmlMemoryLeaks(t)
+    e := Compile(`./*`)
+    if e == nil {
+        t.Error("expr should be good")
+    }
+    e.Free()
+}
